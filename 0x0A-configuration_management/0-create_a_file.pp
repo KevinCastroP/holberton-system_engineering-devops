7d195eecae3414052caa1a07ca2f4c9a2a32c20e
KevCasHSC <castrok.c11@hotmail.com>
2020-03-23 17:58:49 +0000

    last upload

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# Creating a file using puppet
+file { '/tmp/holberton':
+  path    => '/tmp/holberton',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
