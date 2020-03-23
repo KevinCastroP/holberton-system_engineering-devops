b79d83c1b336583c79dcc22a00fb468d46dd8481
KevCasHSC <castrok.c11@hotmail.com>
2020-03-23 21:00:02 +0000

    last upload

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,4 +1,4 @@
-# create a manifest that kills a process named killmenow
+#Ccreate a manifest that kills a process named killmenow
 exec {'killmenow':
      command => 'pkill -f ./killmenow',
      path => ['/usr/bin'],
