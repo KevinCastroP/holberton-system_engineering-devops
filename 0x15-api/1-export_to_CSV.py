b3e6bd022f8f571fd957df62b6c47fda1fb1fbff
KevinCastroP <castrok.c11@hotmail.com>
2020-05-04 17:15:43 -0500

    last update all task

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Exporting data in CSV format
+"""
+
+
+import csv
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    id_em = argv[1]
+    url_employ = "https://jsonplaceholder.typicode.com/users/{}".format(id_em)
+    url_todos = url_employ + "/todos"
+    r_employ = requests.get(url_employ).json()
+    r_todos = requests.get(url_todos).json()
+    username = r_employ.get("username")
+    total_num_task = r_todos
+    list_report = []
+    for task in total_num_task:
+        report = {}
+        report["USER_ID"] = str(task.get("userId"))
+        report["USERNAME"] = str(username)
+        report["TASK_COMPLETED_STATUS"] = str(task.get("completed"))
+        report["TASK_TITLE"] = str(task.get("title"))
+        list_report.append(report)
+    header = ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"]
+    with open("USER_ID.csv", "w") as fcsv:
+        f_csv = csv.DictWriter(fcsv, fieldnames=header, quoting=csv.QUOTE_ALL)
+        f_csv.writerows(list_report)
